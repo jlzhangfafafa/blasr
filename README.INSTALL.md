43fab24949b7dd3362a7cc76f7aca4b7f968afd2
Armin Töpfer <atoepfer@pacificbiosciences.com>
2017-01-24 14:19:50 +0100

    Add cmake

--- README.INSTALL.md
+++ README.INSTALL.md
@@ -1,4 +1,43 @@
-## Installation
+# Installation
+
+## Easy install using cmake and ninja
+
+Make sure that you are using cmake >=3.7 and
+always start from an empty build subdirectory!
+
+    git clone git://github.com/PacificBiosciences/blasr.git && cd blasr
+    git submodule update --init --remote
+    mkdir build && cd build
+    cmake .. && make
+
+Where is the binary?
+
+    blasr/build/blasr
+
+Want to build MUCH faster? Install ninja and execute
+
+    cmake -GNinja .. && ninja
+
+Is your HDF5 in a custom location?
+
+    cmake -GNinja -DHDF5_ROOT=/your/location/hdf-1.8.16 ..
+
+Are HDF$ libraries and include folders in different locations?
+
+    cmake -GNinja -DHDF5_LIBRARIES=/your/location/hdf-1.8.16/lib
+                  -DHDF5_INCLUDE_DIRS=/other/location/hdf-1.8.16/include ..
+
+Prefer a custom libz implementation?
+
+    cmake -GNinja -DZLIB_INCLUDE_DIRS=/your/location/zlib/include \
+                  -DZLIB_LIBRARIES=/your/location/zlib/libz.so ..
+
+Are you on MacOS? Install homebrew and install following dependencies and
+use the default instructions above:
+
+    brew install ninja hdf5 cmake
+
+## Not-so-easy install using make
 
 ### See a step by step Blasr installation example on Blasr wiki page
 
@@ -40,9 +79,9 @@ To build BLASR, you must have hdf 1.8.12 or above installed and
 
   + **HDF5_LIB**, which points to the HDF5 library directory (e.g., hdf5*.a,
   and hdf5*.so)
-  
+
   You may pass arguments to `configure.py` as above, or you may export them from command line:
-  
+
         export HDF5_INC=path_to_your_hdf5_include && export HDF5_LIB=path_to_your_hdf5_lib
 
 * To configure submodule:
---
+++ cmake/blasr-compilerflags.cmake
@@ -0,0 +1,73 @@
+
+include(CheckCXXCompilerFlag)
+
+# shared CXX flags for all source code & tests
+set(BLASR_FLAGS "-std=c++11 -pedantic -Wno-long-long -Wall -Wextra -Wno-return-type -Wno-overloaded-virtual -Wno-unused-parameter -Wno-div-by-zero -MMD -MP -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free -fno-omit-frame-pointer -Wno-unused-variable")
+
+# gperftools support
+if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND APPLE)
+    set(BLASR_LINKER_FLAGS "${BLASR_LINKER_FLAGS} -Wl,-no_pie")
+endif(CMAKE_BUILD_TYPE STREQUAL "Debug" AND APPLE)
+
+# static linking
+IF(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+    set(BLASR_LINKER_FLAGS "${BLASR_LINKER_FLAGS} -static-libgcc -static-libstdc++")
+ENDIF()
+
+# NOTE: quash clang warnings w/ Boost
+check_cxx_compiler_flag("-Wno-unused-local-typedefs" HAS_NO_UNUSED_LOCAL_TYPEDEFS)
+if(HAS_NO_UNUSED_LOCAL_TYPEDEFS)
+    set(BLASR_FLAGS "${BLASR_FLAGS} -Wno-unused-local-typedefs")
+endif()
+
+# Cannot use this until pbbam complies
+# if (CMAKE_COMPILER_IS_GNUCXX)
+#     set(BLASR_FLAGS "${BLASR_FLAGS} -Werror=suggest-override")
+# endif()
+
+# Extra testing that will lead to longer compilation times!
+if (SANITIZE)
+    # AddressSanitizer is a fast memory error detector
+    set(BLASR_SANITY_FLAGS "${BLASR_SANITY_FLAGS} -fsanitize=address -fno-omit-frame-pointer -fno-optimize-sibling-calls")
+
+    # Clang Thread Safety Analysis is a C++ language extension which warns about
+    # potential race conditions in code.
+    set(BLASR_SANITY_FLAGS "${BLASR_SANITY_FLAGS} -Wthread-safety")
+
+    # ThreadSanitizer is a tool that detects data races
+    set(BLASR_SANITY_FLAGS "${BLASR_SANITY_FLAGS} -fsanitize=thread")
+
+    # MemorySanitizer is a detector of uninitialized reads.
+    set(BLASR_SANITY_FLAGS "${BLASR_SANITY_FLAGS} -fsanitize=memory")
+
+    # UndefinedBehaviorSanitizer is a fast undefined behavior detector.
+    set(BLASR_SANITY_FLAGS "${BLASR_SANITY_FLAGS} -fsanitize=undefined")
+endif()
+
+set(BLASR_FLAGS "${BLASR_FLAGS} -DUSE_PBBAM")
+set(BLASR_FLAGS "${BLASR_FLAGS} -DCMAKE_BUILD")
+
+if (HDF5_INCLUDE_DIR)
+    message(FATAL_ERROR "Please specify HDF5_INCLUDE_DIRS not HDF5_INCLUDE_DIR!")
+endif()
+if (HDF5_LIBRARY)
+    message(FATAL_ERROR "Please specify HDF5_LIBRARIES not HDF5_LIBRARY!")
+endif()
+
+if (NOT HDF5_INCLUDE_DIRS OR NOT HDF5_LIBRARIES)
+    set(BLASR_LINKER_FLAGS "${BLASR_LINKER_FLAGS} -L${HDF5_ROOT}/lib")
+    find_package(HDF5 REQUIRED)
+else()
+    set(BLASR_LINKER_FLAGS "${BLASR_LINKER_FLAGS} -L${HDF5_LIBRARIES}")
+    find_library(HDF5_LIBRARIES_     hdf5    ${HDF5_LIBRARIES} NO_CMAKE_SYSTEM_PATH)
+    find_library(HDF5_CPP_LIBRARIES hdf5_cpp ${HDF5_LIBRARIES} NO_CMAKE_SYSTEM_PATH)
+endif()
+
+if (NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    set(BLASR_LINKER_FLAGS "${BLASR_LINKER_FLAGS} -lrt")
+endif()
+
+# shared CXX flags for src & tests
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${BLASR_FLAGS}")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${BLASR_COV_FLAGS} ${BLASR_SANITY_FLAGS}")
+SET(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} ${BLASR_LINKER_FLAGS}")
