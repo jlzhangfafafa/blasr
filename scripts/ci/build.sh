a4116642070d52000d72835a4f4c0b66b258b7d5
David Seifert <dseifert@pacificbiosciences.com>
2018-05-06 20:51:08 +0200

    Make build.sh reusable outside CI

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -1,31 +1,23 @@
 #!/usr/bin/env bash
-set -e
+set -vex
 
-echo "#########"
-echo "# BUILD #"
-echo "#########"
+#########
+# BUILD #
+#########
 
 # in order to make shared libraries consumable
 # by conda and other package managers
 default_LDFLAGS="-static-libstdc++ -static-libgcc"
 
-CURRENT_BUILD_DIR="build"
-
-# 1. configure
+# configure
 # '--wrap-mode nofallback' prevents meson from downloading
 # stuff from the internet or using subprojects.
-echo "## Configuring source (${CURRENT_BUILD_DIR})"
 CPPFLAGS="${HDF5_CFLAGS}" \
 LDFLAGS="${default_LDFLAGS} ${HDF5_LIBS}" \
   meson \
     --wrap-mode nofallback \
     -Dtests="${ENABLED_TESTS:-false}" \
-    "${CURRENT_BUILD_DIR}" .
+    "${CURRENT_BUILD_DIR:-build}" .
 
-# 2. build
-echo "## Building source (${CURRENT_BUILD_DIR})"
-ninja -C "${CURRENT_BUILD_DIR}" -v
-
-# 3. tests
-echo "## Tests (${CURRENT_BUILD_DIR})"
-ninja -C "${CURRENT_BUILD_DIR}" -v test
+# build
+ninja -C "${CURRENT_BUILD_DIR:-build}" -v
